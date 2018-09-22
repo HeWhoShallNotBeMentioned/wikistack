07f2ec7382043137377f8aa4d403d161565802b7
Chris Underwood <cunderwoodmn@gmail.com>
2018-09-22 17:09:11 -0500

    up through section 5 showing a wiki page

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const { Page } = require("../models");
-const { addPage } = require('../views');
+const { Page, User } = require("../models");
+const { addPage, editPage, main, wikiPage  } = require('../views');
 
-router.get('/', (req, res, next) => {
-  res.send('got to GET /WIKI/');
+router.get('/', async (req, res, next) => {
+  try {
+    const pages = await Page.findAll();
+    res.send(main(pages));
+  } catch (error) { next(error) }
 })
 
 router.post('/', async (req, res, next) => {
-  console.log(res.json(req.body));
+  //console.log(res.json(req.body));
   //res.send('got to POST /WIKI/');
 
   const page = new Page ({
     title: req.body.title,
     content: req.body.page_content
   })
-
-  try{
+  console.log("page ", page);
+  try {
     await page.save();
-    res.redirect('/');
+  
+      res.redirect(`/wiki/${page.slug}`);
   } catch (error) {
     next(error)
   }
@@ -27,6 +31,19 @@ router.post('/', async (req, res, next) => {
 router.get('/add', async (req, res, next) => {
   res.send(addPage());
 })
+
+router.get('/:slug', async (req, res, next) => {
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug
+      }
+    });
+    res.send(wikiPage(page));
+  } catch (error) {
+    next(error)
+  }
+});
 
 
 module.exports = router;
