aeeac0ec3263c09b5aab668e0288063dcce97d6a
Chris Underwood <cunderwoodmn@gmail.com>
2018-09-22 19:46:13 -0500

    extra projects for 404 and mark down

--- views/index.js
+++ views/index.js
@@ -4,5 +4,7 @@ const main = require("./main");
 const userList = require("./userList");
 const userPages = require("./userPages");
 const wikiPage = require("./wikiPage");
+const marked = require('marked');
+console.log(marked('I am using __markdown__.'));
 
 module.exports = { addPage, editPage, main, userList, userPages, wikiPage };
